bacdf002e3f6f6bc4a9ac96c468017ac7fb4832d
Sseast <ayadguillaume@gmail.com>
2021-09-12 21:19:12 +0200

    Changed Image tag  as Next requires

--- components/Loading.js
+++ components/Loading.js
@@ -1,14 +1,15 @@
 import { Circle } from "better-react-spinkit";
-
+import Image from "next/image";
 function Loading() {
   return (
     <center style={{ display: "grid", placeItems: "center", height: "100vh" }}>
-      <img
+      <Image
         src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg"
         alt=""
         className=""
         style=""
         height={200}
+        width={200}
         style={{ margin_bottom: 10 }}
       />
       <Circle color="#3CBC28" size={60}></Circle>
